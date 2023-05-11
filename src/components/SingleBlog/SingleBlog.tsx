793fa447f14b17f75dd716e39722585fe3edf633
Muzammil-khan710 <khanmuzammil549@gmail.com>
2023-05-11 18:10:34 +0530

    fixed: console error

--- src/components/SingleBlog/SingleBlog.tsx
+++ src/components/SingleBlog/SingleBlog.tsx
@@ -8,7 +8,7 @@ const SingleBlog = ({data}: Props) => {
   return (
     <section>
         {data.map((item:any) => (
-            <React.Fragment key={item.id}>
+            <React.Fragment key={item.link}>
                 <h1>{item.title}</h1>
                 <p>{item.excerpt}</p>
             </React.Fragment>
--- src/pages/blog/two.tsx
+++ src/pages/blog/two.tsx
@@ -4,18 +4,17 @@ import { GET_POST } from "@/queries/get-post"
 import SingleBlog from "@/components/SingleBlog/SingleBlog"
 
 type Props = {
-    data:any
+    data: any
 }
 
-const Blog = ({data}: Props) => {
+const Blog = ({ data }: Props) => {
     const router = useRouter()
-    console.log(data)
-  return (
-    <div>
-        <h1>Hey this is a blog page and page is {router.asPath} </h1>
-        <SingleBlog data={data}/>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Hey this is a blog page and page is {router.asPath} </h1>
+            <SingleBlog data={data} />
+        </div>
+    )
 }
 
 export default Blog
@@ -24,14 +23,14 @@ export default Blog
 export async function getStaticProps() {
 
     const { data } = await client.query({
-        query:GET_POST
+        query: GET_POST
     })
 
     console.log(data)
 
     return {
         props: {
-            data:data.posts.nodes
+            data: data.posts.nodes
         }
     }
 }
