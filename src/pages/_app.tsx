70e2b6956218e1cc17b697e1c1e386d8799b344a
Muzammil-khan710 <khanmuzammil549@gmail.com>
2023-05-16 11:03:17 +0530

    added nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,14 @@ import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import client from '../apollo/server'
 
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+NProgress.configure( { showSpinner: false } );
+Router.events.on( 'routeChangeStart', () => NProgress.start() );
+Router.events.on( 'routeChangeComplete', () => NProgress.done() );
+Router.events.on( 'routeChangeError', () => NProgress.done() );
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
